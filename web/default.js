8b24f99e794facecd701015791811d445af22897
ewen.zhang <ewen.zhang@welab-inc.com>
2022-12-06 17:28:47 +0800

    feat 默认配置修改

--- web/default.js
+++ web/default.js
@@ -1,11 +1,5 @@
 const defaultConfig = {
-  legend: { y: 'bottom', type: 'scroll' },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow',
-    },
-  },
+  legend: { y: 'bottom' },
   grid: { top: 30, right: 70, bottom: 70, left: 70 },
   animation: false,
 }
